f11de3fb16e67cee5106911f3c2532570639e94f
krishkash94 <135611713+krishkash94@users.noreply.github.com>
2023-11-01 10:10:03 +0530

    Create DSA_1.py
    multiples of 3 and 5

---
+++ DSA_1.py
@@ -0,0 +1,15 @@
+#Write a program which sums the multiples of 3 and 5 until 1000:
+#Code: 
+
+mult_list = []
+for k in range(1, 334):
+    mult = 3 * k
+    mult_list.append(mult)
+print('Sum of multiples of 3 until 3000 is: ',sum(mult_list))
+mult_list1 = []
+for k in range(1, 200):
+    mult = 5 * k
+    mult_list1.append(mult)
+print('Sum of multiples of 5 until 3000 is: ',sum(mult_list1))
+mul_sum = sum(mult_list) + sum(mult_list1)
+print('The sum of multiples of 3 & 5 until 3000 is:',mul_sum)
